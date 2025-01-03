fb1482fcf370db15851c444f3c70daffc2523a43
srivardhan2211 <srivardhanchinthakindi@gmail.com>
2025-01-03 11:35:39 +0530

    Add files via upload

---
+++ example5.java
@@ -0,0 +1,17 @@
+
+import java.util.Scanner;
+
+public class example5 {
+
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.println("Enter your name: ");
+        String name = input.nextLine();
+        System.out.println("Enter your age:");
+        int age = input.nextInt();
+        System.out.println("Enter your phonenumber:");
+        String phonenumber = input.nextLine();
+
+    }
+
+}
