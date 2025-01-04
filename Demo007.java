6400668212aefa9f17179dcf77fb4788537dc7a5
srivardhan2211 <srivardhanchinthakindi@gmail.com>
2025-01-04 09:29:50 +0530

    Add files via upload

---
+++ Demo007.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+public class Demo007{
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+            System.out.println("Enter Your Name: ");
+            String Name= sc.next();
+            System.out.println("Enter your Age: ");
+            int Age= sc.nextInt();
+            System.out.println("Enter your Phone : ");
+            String Phone= sc.next();
+
+            
+        
+    }
+}
