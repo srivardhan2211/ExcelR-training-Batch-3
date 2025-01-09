cb1746efe8271a821848c734510b5c0dc77e9dd4
srivardhan2211 <srivardhanchinthakindi@gmail.com>
2025-01-09 12:29:55 +0530

    Add files via upload

---
+++ FizzBizz.java
@@ -0,0 +1,24 @@
+ public class FizzBizz {
+    public static String getFizzBizz(int num) {
+        if (num <= 0) {
+            return "Error";
+        } else if (num % 3 == 0 && num % 5 == 0) {
+            return "FIZZBIZZ";
+        } else if (num % 3 == 0) {
+            return "FIZZ";
+        } else if (num % 5 == 0) {
+            return "BIZZ";
+        } else {
+            return String.valueOf(num);
+        }
+    }   
+
+    public static void main(String[] args) {
+        System.out.println(getFizzBizz(33));  
+        System.out.println(getFizzBizz(5));   
+        System.out.println(getFizzBizz(15));  
+        System.out.println(getFizzBizz(7));   
+        System.out.println(getFizzBizz(-1));  
+        System.out.println(getFizzBizz(0));   
+    }
+}
