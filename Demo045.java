4dc31beb382c35ac3c665f554f00d8c513b0a1b1
srivardhan2211 <srivardhanchinthakindi@gmail.com>
2025-01-25 12:46:43 +0530

    snapping two numbers without third variable

---
+++ Demo045.java
@@ -0,0 +1,19 @@
+public class Demo045 {
+    public static void main(String[] args) {
+        int num1 = 10;
+        int num2 = 20;
+
+        System.out.println("Before swapping:");
+        System.out.println("num1 = " + num1);
+        System.out.println("num2 = " + num2);
+
+        // Swapping without using a third variable
+        num1 = num1 + num2;
+        num2 = num1 - num2;
+        num1 = num1 - num2;
+
+        System.out.println("After swapping:");
+        System.out.println("num1 = " + num1);
+        System.out.println("num2 = " + num2);
+    }
+}
