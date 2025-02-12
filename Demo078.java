ef5b6dfe400dd57949d4bae5088ae9ff581fefc7
srivardhan2211 <srivardhanchinthakindi@gmail.com>
2025-02-12 13:00:05 +0530

    printing even numbers in left side and odd numbers in right

---
+++ Demo078.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+public class Demo078 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter the size of the array:");
+        int n = scanner.nextInt();
+        int[] arr = new int[n];
+        System.out.println("Enter the elements of the array:");
+        for (int i = 0; i < n; i++) {
+            arr[i] = scanner.nextInt();
+        }
+        System.out.println("Even numbers: ");
+        for (int i = 0; i < n; i++) {
+            if (arr[i] % 2 == 0) {
+                System.out.print(arr[i] + " ");
+            }
+        }
+        System.out.println("\nOdd numbers: ");
+        for (int i = 0; i < n; i++) {
+            if (arr[i] % 2 != 0) {
+                System.out.print(arr[i] + " ");
+            }
+        }
+    }
+}
