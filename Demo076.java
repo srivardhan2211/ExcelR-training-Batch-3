b469f428e0d92c4e602f9c29c8b6df231308f763
srivardhan2211 <srivardhanchinthakindi@gmail.com>
2025-02-12 12:59:17 +0530

    Second max element in array

---
+++ Demo076.java
@@ -0,0 +1,30 @@
+
+import java.util.*;
+public class Demo076 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter the size of the array:");
+        int n = scanner.nextInt();
+        int[] arr = new int[n];
+        System.out.println("Enter the elements of the array:");
+        for (int i = 0; i < n; i++) {
+            arr[i] = scanner.nextInt();
+        }
+        int max = Integer.MIN_VALUE;
+        int secondMax = Integer.MIN_VALUE;
+
+        for (int i = 0; i < n; i++) {
+            if (arr[i] > max) {
+                secondMax = max;
+                max = arr[i];
+            } else if (arr[i] > secondMax && arr[i] != max) {
+                secondMax = arr[i];
+            }
+        }
+        if (secondMax == Integer.MIN_VALUE) {
+            System.out.println("There is no second maximum number.");
+        } else {
+            System.out.println("The second maximum number is: " + secondMax);
+        }
+    }
+}
