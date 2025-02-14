38c4aac23acdaed0b76fe219c4f8a0b57c7e95e7
srivardhan2211 <srivardhanchinthakindi@gmail.com>
2025-02-14 12:02:35 +0530

    shifting array elements to left side and first element to last.

---
+++ Demo080.java
@@ -0,0 +1,28 @@
+import java.util.Scanner;
+public class Demo080 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter the size of the array:");
+        int n = scanner.nextInt();
+        int[] arr = new int[n];
+        System.out.println("Enter the elements of the array:");
+        for (int i = 0; i < n; i++) {
+            arr[i] = scanner.nextInt();
+        }
+        System.out.println("Original array:");
+        printArray(arr);
+        int firstElement = arr[0];
+        for (int i = 0; i < n - 1; i++) {
+            arr[i] = arr[i + 1];
+        }
+        arr[n - 1] = firstElement;
+        System.out.println("Array after shifting:");
+        printArray(arr);
+    }
+    public static void printArray(int[] arr) {
+        for (int i = 0; i < arr.length; i++) {
+            System.out.print(arr[i] + " ");
+        }
+        System.out.println();
+    }
+}
